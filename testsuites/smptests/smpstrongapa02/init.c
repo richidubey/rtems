467a164ce174446e2c89c7e7de4930789593081b
richidubey <richidubey@gmail.com>
2020-07-27 18:14:44 +0530

    Added basic files and introduction

---
+++ testsuites/smptests/smpstrongapa02/init.c
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Richi Dubey 
+ * All rights reserved.
+ *
+ * richidubey@gmail.com
+ *
+ * The license and distribution terms for this file may be
+ * found in the file LICENSE in this distribution or at
+ * http://www.rtems.org/license/LICENSE.
+ */
+
+
